d510783139d99b483231b02b565ca81ec67dcbae
igntrevor <igntrevor@gmail.com>
2026-01-28 21:29:56 +0300

    stub: add segmentation entry point to be rebuilt from notebook

---
+++ src/data/segmentation.py
@@ -0,0 +1,5 @@
+def segment_dataset(raw_path, processed_path, time_ms, devices):
+    raise NotImplementedError(
+        "segment_dataset is intentionally not implemented yet. "
+        "It will be rebuilt step-by-step from Notebook 1."
+    )
